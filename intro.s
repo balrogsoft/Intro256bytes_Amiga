337422f00bb41019b5de091d57ac2d31b3ebe6b7
balrogsoft <balrogsoft@gmail.com>
2026-07-01 20:40:21 +0200

    Update intro.s
    Optimized in size, 96 bytes of executable, 160 bytes free for your effect.

--- intro.s
+++ intro.s
@@ -1,57 +1,36 @@
 ; 256 bytes intro skeleton for Amiga 500 with KS 1.3
-; with 144 bytes free for your effect
+; with 160 bytes free for your effect
 ; by Balrog Soft / Amiga Skool
 
 ib_ActiveScreen	EQU 56
-gb_ActiView 	EQU 34
-v_ViewPort 		EQU 0
-vp_RasInfo		EQU 36
-sc_ViewPort		EQU 44
-ri_BitMap		EQU 4
+rp_BitMap		EQU 88
 bm_Planes		EQU 8
-_LVOLoadRGB4    EQU -192
 
-    move.w  #32,$dff096				; Disable mouse pointer
+	move.w  #32,$dff096             ; Disable mouse pointer (DMACON)
 
-    move.l  $170(a2),a6            	; IntuitionBase from globvec
-    move.l  ib_ActiveScreen(a6),a5	; a5 = ActiveScreen (640x256 on most systems)
-
-    move.l  $4.w,a6					; ExecBase
-    move.l  156(a6),a6				; graphics.library
-
-    move.l  gb_ActiView(a6),a2		; Get ActiveView
-
-    move.l  v_ViewPort(a2),a2		; Get ViewPort
-
-    move.l  vp_RasInfo(a2),a2		; Get RasInfo
-
-    move.l  ri_BitMap(a2),a2		; Get screen Bitmap
-
-    ;lea     sc_ViewPort(a5),a0		; Set colors
-    ;lea     palette(pc),a1
-    ;moveq   #4,d0
-    ;jsr     _LVOLoadRGB4(a6)
+    move.l  $170(a2),a6             ; a6 = IntuitionBase from globvec
+    move.l  ib_ActiveScreen(a6),a0  ; a0 = ib_ActiveScreen
+    move.l  rp_BitMap(a0),a0        ; a0 = rp_BitMap
 	
 frameloop:
-    move.l  #255,d7                 ; y
+    move.w  #255,d7               	; y
 
-palette:
-    move.l  bm_Planes(a2),a3		; Bitplane 0 to a3
-    move.l  bm_Planes+4(a2),a4		; Bitplane 1 to a4
+    move.l  bm_Planes(a0),a3        ; Bitplane 0 to a3
+    move.l  bm_Planes+4(a0),a4      ; Bitplane 1 to a4
 
 yLoop:
-    moveq	#19,d6                  ; x
-	
+    moveq   #19,d6                  ; x
+    
 xLoop:
-	; write data to bitplanes
-	;move.l  #$00ff00ff,(a3)+
+    ; write data to bitplanes
+    ;move.l  #$00ff00ff,(a3)+
     ;move.l  #$0ff00ff0,(a4)+
-	
-    dbra	d6,xLoop
-	
-    dbra	d7,yLoop
-	
-	btst	#6,$bfe001				; Check for mouse left click button to exit
+    
+    dbra    d6,xLoop
+    
+    dbra    d7,yLoop
+    
+    btst    #6,$bfe001              ; Check for mouse left click button to exit
     bne.s   frameloop
-	moveq	#0,d0
+    moveq   #0,d0
     rts
